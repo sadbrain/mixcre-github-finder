7a106589eee59c00ca9f9d42df1e2175cf2f5cfb
sadbrain <riotgheptrannhulol@gmail.com>
2024-05-19 14:36:02 +0700

    6.feat: create search component and handle api

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users'
+import Search from './components/users/Search';
 function App() {
-   const [users, setUsers] = useState([]);
-
-   useEffect(() => {
-      const fetchData = async () => {
-         try {
-            const response = await axios.get('https://api.github.com/users');
-            setUsers(response.data); 
-         } catch (error) {
-            console.error('Error fetching data:', error);
-         }
-      };
-      fetchData();
-   }, []);
+ 
    return (
       <div className="App">
          <Navbar />
          <div className="container">
             <h1>GitHub Users Data</h1>
+            <Search />
          </div>
-         <Users users={users} />
       </div>
    );
 }
