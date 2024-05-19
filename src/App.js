de95b389be4f363a04bd64b64076163be30cb0bd
sadbrain <riotgheptrannhulol@gmail.com>
2024-05-19 14:21:53 +0700

    4.feat: fecth user data with useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import axios from "axios";
+import { useEffect } from "react";
 import './App.css';
 import Navbar from "./components/layout/Navbar";
 
 function App() {
+  useEffect(()=>{
+   const fetchData = async () => {
+      try {
+      const response = await axios.get("https://api.github.com/users");
+      console.log("GitHub Users:", response.data);
+      } catch (error) {
+      console.error("Error fetching data:", error);
+      }
+      };
+      fetchData();
+  },[])
    return (
       <div className="App">
          <Navbar />
