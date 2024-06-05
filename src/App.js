2610706cf59b35eaed966d82748c8d7c890bc72e
sadbrain <riotgheptrannhulol@gmail.com>
2024-06-05 12:12:32 +0700

    feat: finish project

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, {} from "react";
 import { BrowserRouter as Router } from "react-router-dom"; import "./App.css";
 import Home from "./components/layout/Home";
 import Navbar from "./components/layout/Navbar";
-import { TextContext, TextProvider } from './components/TextContext';
-import { ThemeProvider, ThemeContext } from './components/ThemeContext';
+import { TextProvider } from './components/TextContext';
+import { ThemeProvider } from './components/ThemeContext';
 
 const App = () => {
    return (
